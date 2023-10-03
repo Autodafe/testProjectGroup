dfae0106a91666248c424f059e59c1245b299936
Autodafe <necome@mail.ru>
2023-10-03 16:50:48 +0300

    upd ajax

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 import { initBurgerButton } from './burger-menu.js';
-import { initDescriptionText } from './init-description.js'
+import { initDescriptionText } from './init-description.js';
 
 initBurgerButton();
 initDescriptionText();
